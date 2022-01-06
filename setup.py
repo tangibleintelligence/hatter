68e7410f38a7fbbd4471956d6e77e47707c4f255
Austin Howard <austin@tangibleintelligence.com>
2022-01-06 16:05:27 -0600

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", encoding="utf-8") as readme:
 
 setup(
     name="hatter",
-    version="0.2.0",
+    version="0.3.0",
     description="Framework to easily create microservices backed by a RabbitMQ broker",
     long_description=LONG_DESCRIPTION,
     long_description_content_type="text/markdown",
